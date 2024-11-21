29afaa0689078029a9a2200824ccd1127b70f34d
Lucas Aguirre <lucasaguirreescuela@gmail.com>
2024-11-21 11:07:29 -0300

    change vercel json options

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/navbar';
 import Bienvenida from './components/bienvenida';
@@ -21,12 +21,12 @@ function App() {
 }
 
 const MainContent: React.FC = () => {
-  const location = useLocation();
+
   return (
     <>
       <Navbar/>
-      {location.pathname === '/' && <Bienvenida />}
       <Routes>
+        <Route path="/" element={<Bienvenida />} />
         <Route path="/challenges" element={ <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -50 }} transition={{ duration: 0.5 }}><Challenges /></motion.div>} />
         <Route path="/contenido" element={<motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -50 }} transition={{ duration: 0.5 }}><Contenido /></motion.div>} />
          <Route path="/only" element={<motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -50 }} transition={{ duration: 0.5 }}><Only /></motion.div>} />
